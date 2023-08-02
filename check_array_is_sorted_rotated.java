9123d9ce032f196926a613ceb691471177c69602
Rohanbeniwal2307 <rohanbeniwal2001@gmail.com>
2023-08-02 12:32:29 +0530

    complete brute to optimal approch

---
+++ check_array_is_sorted_rotated.java
@@ -0,0 +1,32 @@
+//brute force 
+
+class Btute_force {
+  static boolean isSorted(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+      for (int j = i + 1; j < n; j++) {
+        if (arr[j] < arr[i])
+          return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+//optimal 
+class Solution {
+    public boolean check(int[] A) {
+       int k = 0, n = A.length;
+        for (int i = 0; i < n; ++i) {
+            if (A[i] > A[(i + 1) % n]) {//  "%n" is making a array simple to rotated
+                k++;
+            }
+            if (k > 1) { //greater than 1 isliye h kyunki hum 1 tak  bear kr sakte h kyunki jb wo
+                // rotated array jb khni khtm ho rhi hogi to element wo usse smaller rhega na 
+                //isliye bs 1 hoga 
+                return false;
+            }
+        }
+        return true;
+    }
+}
