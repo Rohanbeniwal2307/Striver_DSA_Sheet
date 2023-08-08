357da3a99aab40b4cbbda829b9aa095c4e6f21a5
Rohanbeniwal2307 <rohanbeniwal2001@gmail.com>
2023-08-09 00:07:49 +0530

    brute and hashing approch

--- Two_Sum.java
+++ Two_Sum.java
@@ -1,4 +1,6 @@
-/*  // brute
+  // brute
+  // Tc-->=N*2
+ import java.util.*;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
@@ -11,10 +13,23 @@ class Solution {
         return new int[] {};
     }
 }
-        //optimal
-import java.util.HashMap;
-import java.util.Map;
-class Solution {
+
+//optimal..by using hashmap 
+/*  Time Complexity: O(N), where N = size of the array.
+Reason: The loop runs N times in the worst case and searching in a hashmap takes O(1) generally. So the time complexity is O(N).
+
+Note: In the worst case(which rarely happens), the unordered_map takes O(N) to find an element. 
+In that case, the time complexity will be O(N2). If we use map instead of unordered_map, 
+the time complexity will be O(N* logN) as the map data structure takes logN time to find an element.
+
+Space Complexity: O(N) as we use the map data structure.
+
+Note: We have optimized this problem enough. But if in the interview, we are not allowed to use
+ the map data structure, then we should move on to the following approach i.e. two pointer approach.
+  This approach will have the same time complexity as the better approach.*/
+
+
+class Solution2 {
     public int[] twoSum(int[] nums, int target)
      {
         Map<Integer,Integer> map=new HashMap<>();
@@ -27,6 +42,6 @@ class Solution {
         map.put(nums[j],j);
         }
         return new int[] {};
-    }
+     }
 }
-*/
+
---
+++ longest_subarray_ofSumk_Pos.java
@@ -0,0 +1,115 @@
+
+    
+
+// brute force 
+// Tc--> N*3
+import java.util.*;
+
+public class longest_subarray_ofSumk_Pos {
+    public static int getLongestSubarray(int []a, long k) {
+        int n = a.length; // size of the array.
+
+        int len = 0;
+        for (int i = 0; i < n; i++) { 
+            for (int j = i; j < n; j++) { 
+                
+                long s = 0;
+                for (int K = i; K <= j; K++) {
+                    s += a[K];
+                }
+
+                if (s == k)
+                    len = Math.max(len, j - i + 1);
+            }
+        }
+        return len;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {2, 3, 5, 1, 9};
+        long k = 10;
+        int len = getLongestSubarray(a, k);
+        System.out.println("The length of the longest subarray is: " + len);
+    }
+    
+
+
+// Better 1
+// Tc --> n*2
+
+public class Better1 {
+    public static int getLongestSubarray(int []a, long k) {
+        int n = a.length; 
+
+        int len = 0;
+        for (int i = 0; i < n; i++) { 
+            long s = 0; 
+            for (int j = i; j < n; j++) {
+            
+                s += a[j];
+
+                if (s == k)
+                    len = Math.max(len, j - i + 1);
+            }
+        }
+        return len;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {2, 3, 5, 1, 9};
+        long k = 10;
+        int len = getLongestSubarray(a, k);
+        System.out.println("The length of the longest subarray is: " + len);
+    }
+}
+
+// solution 3 using hashmap 
+
+
+
+
+
+
+
+// soln 4 
+// Time Complexity: O(2*N), where N = size of the given array.
+//Reason: The outer while loop i.e. the right pointer can move up to index n-1(the last index). Now, the inner while loop i.e. the left pointer can move up to the right pointer at most. So, every time the inner loop does not run for n times rather it can run for n times in total. So, the time complexity will be O(2*N) instead of O(N2).
+
+
+//optimal approch using 2 pointer
+public class Better3 {
+    public static int getLongestSubarray(int []a, long k) {
+        int n = a.length; 
+
+        int left = 0, right = 0; 
+        long sum = a[0];
+        int maxLen = 0;
+        while (right < n) {
+          
+            while (left <= right && sum > k) {
+                sum -= a[left];
+                left++;
+            }
+
+            if (sum == k) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+            right++;
+            if (right < n) sum += a[right];
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {2, 3, 5, 1, 9};
+        long k = 10;
+        int len = getLongestSubarray(a, k);
+        System.out.println("The length of the longest subarray is: " + len);
+    }
+}
+
+}
+
+
+
