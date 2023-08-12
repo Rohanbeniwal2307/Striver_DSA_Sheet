4c73878e23153fbaf956eb17d094a2d4c59b9a08
Rohanbeniwal2307 <rohanbeniwal2001@gmail.com>
2023-08-12 15:37:35 +0530

    brute better to kadane's algo

---
+++ mxm_sum_of_subarray.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+
+// brute force 
+// Time Complexity: O(N3)
+class Solution1brute {
+    public int maxSubArray(int[] nums) {
+    
+        int n=nums.length;
+        int maxi=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            
+            for(int j=i;j<n;j++){
+                int sum=0;
+                for(int k=i;k<=j;k++){
+                    sum=sum+nums[k];
+                    
+                }
+                maxi=Math.max(sum,maxi);
+                
+                
+            }
+        }
+        return maxi;
+    }
+}
+// Time Complexity: O(N2)
+class Solution2better {
+    public int maxSubArray(int[] nums) {
+    
+        int n=nums.length;
+        int maxi=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=i;j<n;j++){
+                sum=sum+nums[j];
+                
+                maxi=Math.max(sum,maxi);
+                
+                
+            }
+        }
+        return maxi;
+    }
+}
+// Time Complexity: O(N)
+class Solution3optimal {
+    public int maxSubArray(int[] nums) {
+        int n=nums.length;
+        int sum=0;
+        int maxi=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            sum=sum+nums[i];
+            if(sum>maxi){
+                maxi=sum;
+            }
+            if(sum<0) sum=0;
+        }
+        return maxi;
+    }
+}
